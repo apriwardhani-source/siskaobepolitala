c076d19887e64c946806c0177deb71e786209be6
nadiva@mhs.politala.ac.id <nadiva@mhs.politala.ac.id>
2025-10-02 09:26:17 +0800

    wadir1

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
@@ -14,19 +13,28 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
+        // Admin
+        $admin = User::factory()->create([
             'name' => 'Admin Apri',
-            'email' => 'apri@politala.ac.id', // Ganti dengan email admin kamu
-            'password' => Hash::make('Wrrdhnii16'), // Ganti dengan password kuat
-            'role' => 'admin',
+            'email' => 'apri@politala.ac.id',
+            'password' => Hash::make('Wrrdhnii16'),
         ]);
+        $admin->assignRole('Admin');
 
         // Kaprodi
-        User::factory()->create([
+        $kaprodi = User::factory()->create([
             'name' => 'Kaprodi TI',
-            'email' => 'kaprodi@politala.ac.id', // email kaprodi
-            'password' => Hash::make('Kaprodi123'), // password kaprodi
-            'role' => 'kaprodi',
+            'email' => 'kaprodi@politala.ac.id',
+            'password' => Hash::make('Kaprodi123'),
         ]);
+        $kaprodi->assignRole('Kaprodi');
+
+        // Wadir 1
+        $wadir = User::factory()->create([
+            'name' => 'Wadir 1',
+            'email' => 'wadir1@politala.ac.id',
+            'password' => Hash::make('Wadir123'),
+        ]);
+        $wadir->assignRole('Wadir 1');
     }
 }
